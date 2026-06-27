58e2909968044e29f0802cbb3f66ebba9d186a6c
Bharadwaja1557 <bharadwaja1557@gmail.com>
2026-06-28 00:42:24 +0530

    path fix

--- data/movies.js
+++ data/movies.js
@@ -10,7 +10,36 @@
 // Fields: title, year (release year), watchedDate, language (shown as a
 // tag), poster (path), and optional "rerelease": true for a Re-release pill.
 
-window.ACMovies = [
+window.ACMovies = 
+[
+  {
+    "title": "Baahubali: The Epic",
+    "year": 2025,
+    "watchedDate": "2025-10-31",
+    "language": "Telugu",
+    "poster": "posters/BaahubaliTheEpic(2025).jpg"
+  },
+  {
+    "title": "Kantara: Chapter 1",
+    "year": 2025,
+    "watchedDate": "2025-10-02",
+    "language": "Kannada",
+    "poster": "posters/KantaraChapter1(2025).jpg"
+  },
+  {
+    "title": "Captain America: Brave New World",
+    "year": 2025,
+    "watchedDate": "2025-02-14",
+    "language": "English",
+    "poster": "posters/CaptainAmericaBraveNewWorld(2025).jpg"
+  },
+  {
+    "title": "Miss Shetty Mr Polishetty",
+    "year": 2023,
+    "watchedDate": "2023-09-07",
+    "language": "Telugu",
+    "poster": "posters/MissShettyMrPolishetty(2023).jpg"
+  },
   {
     "title": "Oppenheimer",
     "year": 2023,
@@ -23,14 +52,50 @@ window.ACMovies = [
     "year": 2022,
     "watchedDate": "2022-07-08",
     "language": "English",
-    "poster": "posters/Thor Love and Thunder(2022).jpg"
+    "poster": "posters/ThorLoveandThunder(2022).png"
   },
   {
-    "title": "Captain America: Brave New World",
-    "year": 2025,
-    "watchedDate": "2025-02-14",
-    "language": "English",
-    "poster": "posters/Captain America Brave New World(2025).jpg"
+    "title": "Kantara",
+    "year": 2022,
+    "watchedDate": "2022-10-15",
+    "language": "Kannada",
+    "poster": "posters/Kantara(2022).jpg"
+  },
+  {
+    "title": "Manchi Rojulochaie",
+    "year": 2021,
+    "watchedDate": "2021-12-25",
+    "language": "Telugu",
+    "poster": "posters/ManchiRojulochaie(2021).jpg"
+  },
+  {
+    "title": "Baahubali 2: The Conclusion",
+    "year": 2017,
+    "watchedDate": "2017-04-28",
+    "language": "Telugu",
+    "poster": "posters/Baahubali2TheConclusion(2017).jpg"
+  },
+  {
+    "title": "Baahubali: The Beginning",
+    "year": 2015,
+    "watchedDate": "2015-07-10",
+    "language": "Telugu",
+    "poster": "posters/BaahubaliTheBeginning(2015).jpg"
+  },
+  {
+    "title": "Seethamma Vakitlo Sirimalle Chettu",
+    "year": 2013,
+    "watchedDate": "2025-01-11",
+    "language": "Telugu",
+    "rerelease": true,
+    "poster": "posters/SeethammaVakitloSirimalleChettu(2013).jpg"
+  },
+  {
+    "title": "Shakti",
+    "year": 2011,
+    "watchedDate": "2011-09-23",
+    "language": "Telugu",
+    "poster": "posters/Shakti(2011).jpg"
   },
   {
     "title": "Khaleja",
@@ -41,76 +106,11 @@ window.ACMovies = [
     "poster": "posters/Khaleja(2010).jpg"
   },
   {
-    "title": "Seethamma Vakitlo Sirimalle Chettu",
-    "year": 2013,
-    "watchedDate": "2025-01-11",
-    "language": "Telugu",
-    "rerelease": true,
-    "poster": "posters/Seethamma Vakitlo Sirimalle Chettu(2013).jpg"
-  },
-  {
-    "title": "Shakti",
-    "year": 2011,
-    "watchedDate": "2011-09-23",
-    "language": "Telugu",
-    "poster": "posters/Shakti(2011).jpg"
-  },
-  {
-    "title": "Baahubali: The Beginning",
-    "year": 2015,
-    "watchedDate": "2015-07-10",
-    "language": "Telugu",
-    "poster": "posters/Baahubali The Beginning(2015).jpg"
-  },
-  {
-    "title": "Baahubali 2: The Conclusion",
-    "year": 2017,
-    "watchedDate": "2017-04-28",
-    "language": "Telugu",
-    "poster": "posters/Baahubali 2 The Conclusion(2017).jpg"
-  },
-  {
-    "title": "Baahubali: The Epic",
-    "year": 2025,
-    "watchedDate": "2025-10-31",
-    "language": "Telugu",
-    "rerelease": true,
-    "poster": "posters/Baahubali The Epic(2025).jpg"
-  },
-  {
-    "title": "Kantara",
-    "year": 2022,
-    "watchedDate": "2022-10-15",
-    "language": "Kannada",
-    "poster": "posters/Kantara(2022).jpg"
-  },
-  {
-    "title": "Kantara: Chapter 1",
-    "year": 2025,
-    "watchedDate": "2025-10-02",
-    "language": "Kannada",
-    "poster": "posters/Kantara Chapter 1(2025).jpg"
-  },
-  {
-    "title": "Miss Shetty Mr Polishetty",
-    "year": 2023,
-    "watchedDate": "2023-09-07",
-    "language": "Telugu",
-    "poster": "posters/Miss Shetty Mr Polishetty(2023).jpg"
-  },
-  {
-    "title": "Racha",
+    "title": "Rachcha",
     "year": 2012,
     "watchedDate": "2012-03-30",
     "language": "Telugu",
-    "poster": "posters/Racha(2012).jpg"
-  },
-  {
-    "title": "Manchi Rojulochaie",
-    "year": 2021,
-    "watchedDate": "2021-12-25",
-    "language": "Telugu",
-    "poster": "posters/Manchi Rojulochaie(2021).jpg"
+    "poster": "posters/Rachcha(2012).jpg"
   }
 ]
 ;
